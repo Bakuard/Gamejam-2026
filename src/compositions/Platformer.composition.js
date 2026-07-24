c2d64212fc1912c786648515688d89d396000ab9
Huns Bakuard <purplespicemerchant@gmail.com>
2026-07-25 02:19:57 +0700

    Декор

--- src/compositions/Platformer.composition.js
+++ src/compositions/Platformer.composition.js
@@ -3,6 +3,7 @@ import {tilemapComposition } from "@/compositions/Tilemap.composition.js";
 export const platformerComposition = {
   preloadLevel(scene) {
     scene.load.image("floor-wall-roof", "assets/levels/tiles/floor-wall-roof.png");
+    scene.load.image("decor", "assets/levels/tiles/level-art-tiles.png");
     scene.load.image("chair", "assets/levels/tiles/chair.png");
     scene.load.tilemapTiledJSON("platformer-tilemap", "assets/levels/tilemaps/platformer.json");
     scene.load.image('mountBack', 'assets/img/background/mount-back.png');
@@ -12,6 +13,8 @@ export const platformerComposition = {
   createLevel(scene) {
     const map = scene.make.tilemap({ key: "platformer-tilemap" });
 
+    tilemapComposition.createTileLayer(map, "decor", "BackgroundWalls", []);
+    tilemapComposition.createTileLayer(map, "decor", "DecorItems", []);
     const platformLayer = tilemapComposition.createTileLayer(map, "floor-wall-roof", "Platforms", [4, 5, 6]);
     const woodPlatformLayer = tilemapComposition.createTileLayer(map, "floor-wall-roof", "WoodPlatforms", [4, 5, 6], true);
     const wallsLayer = tilemapComposition.createTileLayer(map, "floor-wall-roof", "Walls", [4, 5, 6]);
@@ -49,6 +52,6 @@ export const platformerComposition = {
     const scrollY = camera.scrollY;
 
     backgroundFar.setPosition(-scrollX * 0.3, scene.scale.height - scrollY * 0.55);
-    backgroundNear.setPosition(-scrollX * 0.7, scene.scale.height - scrollY * 0.8);
+    backgroundNear.setPosition(-scrollX * 0.7, scene.scale.height - scrollY * 0.9);
   }
 };
