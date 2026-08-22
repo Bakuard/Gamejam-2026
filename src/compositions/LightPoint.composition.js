a191765e375bd1d1681f32d782dd06de01326a7b
Huns Bakuard <purplespicemerchant@gmail.com>
2026-08-22 15:38:26 +0600

    Заженный источник света со временм потухает

--- src/compositions/LightPoint.composition.js
+++ src/compositions/LightPoint.composition.js
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
 import { inventoryComposition } from "@/compositions/Inventory.composition.js";
-import { ITEM_MATCHES } from "@/configs/gameplay.config.js";
+import { ITEM_MATCHES, LIGHT_POINT } from "@/configs/gameplay.config.js";
 
 export const lightPointComposition = {
   preloadLightPointAnimation(scene) {
@@ -14,14 +14,25 @@ export const lightPointComposition = {
       lightPoint.setOrigin(0, 1);
       lightPoint.setDisplaySize(lightPointMeta.width, lightPointMeta.height);
       lightPoint.refreshBody();
+      lightPoint.currentBurningTimeInMs = 0;
     });
     return lightPointsPhysicLayer;
   },
 
   interactWithLightPoint(inventoryStore, lightPoint, userInput) {
-    Phaser.Input.Keyboard.JustDown(userInput.interact)
-      && inventoryComposition.decreaseItem(inventoryStore, ITEM_MATCHES)
-      && lightPoint.setFrame("2");
+    const turnOn = lightPoint.currentBurningTimeInMs <= 0 && Phaser.Input.Keyboard.JustDown(userInput.interact) && inventoryComposition.decreaseItem(inventoryStore, ITEM_MATCHES);
+
+    if (turnOn) {
+      lightPoint.setFrame("2");
+      lightPoint.currentBurningTimeInMs = LIGHT_POINT.maxBurningTimeInSec * 1000;
+    }
+  },
+
+  decreaseBurningTime(lightPointsLayer, deltaTime) {
+    lightPointsLayer.getChildren().forEach(lightPoint => {
+      if (lightPoint.currentBurningTimeInMs > 0) lightPoint.currentBurningTimeInMs -= deltaTime;
+      else lightPoint.setFrame("1");
+    });
   },
 };
 
