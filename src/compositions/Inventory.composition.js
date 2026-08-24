2d686bea63fbddd50f9394e266d42bdd652908a9
Huns Bakuard <purplespicemerchant@gmail.com>
2026-08-24 14:22:28 +0600

    Убрано ограничение на размер стаков

--- src/compositions/Inventory.composition.js
+++ src/compositions/Inventory.composition.js
@@ -1,15 +1,7 @@
-import { INVENTORY } from "@/configs/gameplay.config.js";
-
 export const inventoryComposition = {
   increaseItem(inventoryStore, itemType) {
-    let inventoryItem = inventoryStore.items.find((item) => item.name === itemType && item.amount < INVENTORY.maxStackSize);
-
-    if (!inventoryItem && inventoryStore.items.length === INVENTORY.maxStacks) return false;
-
-    if (inventoryItem) inventoryItem.amount++;
-    else inventoryStore.items.push({ name: itemType, amount: 1 });
-
-    return true;
+    let inventoryItem = inventoryStore.items.find((item) => item.name === itemType);
+    inventoryItem.amount++;
   },
 
   decreaseItem(inventoryStore, itemType) {
@@ -17,7 +9,6 @@ export const inventoryComposition = {
     if (inventoryItemIndex !== -1) {
       const item = inventoryStore.items[inventoryItemIndex];
       item.amount--;
-      if (item.amount === 0) inventoryStore.items.splice(inventoryItemIndex, 1);
       return true;
     }
     return false;
